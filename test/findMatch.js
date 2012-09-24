3d67c42f067e9601708a2070bfa1d469874647ea
Nate Silva <nate@desi.com>
2012-09-24 12:09:59 -0700

    add tests for the findMatch module

---
+++ test/findMatch.js
@@ -0,0 +1,56 @@
+var test = require('tap').test
+  , findMatch = require('../lib/docserver/findMatch.js')
+  ;
+
+
+test('resolve a target that has no extension', function(t) {
+  findMatch('docs/hello', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.equal(result, 'docs/hello.md', 'should resolve to hello.md');
+    t.end();
+  });
+});
+
+test('resolve a target that has a known extension', function(t) {
+  findMatch('docs/goodbye/index.mdown', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.equal(result, 'docs/goodbye/index.mdown',
+      'should resolve to index.mdown');
+    t.end();
+  });
+});
+
+test('resolve a target that is a directory', function(t) {
+  findMatch('docs/goodbye', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.equal(result, 'docs/goodbye/index.mdown',
+      'should resolve to index.mdown');
+    t.end();
+  });
+});
+
+test('resolve a target that does not exist', function(t) {
+  findMatch('docs/foo', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.notOk(result, 'result should be empty');
+    t.end();
+  });
+});
+
+test('resolve a target with a leading dot', function(t) {
+  findMatch('./docs/hello', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.equal(result, 'docs/hello.md',
+      'should resolve to hello.md without a dot (normalized)');
+    t.end();
+  });
+});
+
+
+test('resolve a target with an unknown extension', function(t) {
+  findMatch('./docs/hello.getdown', function(err, result) {
+    t.notOk(err, 'should complete without error');
+    t.notOk(result, 'result should be empty');
+    t.end();
+  });
+});
