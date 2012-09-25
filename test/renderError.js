ecba6340fe02d4b107ca4dab590c394594ac2dda
Nate Silva <nate@desi.com>
2012-09-25 09:50:44 -0700

    separate rendering an error page and sending it to the client

---
+++ test/renderError.js
@@ -0,0 +1,37 @@
+var test = require('tap').test
+  , fs = require('fs')
+  , renderError = require('../lib/docserver/renderError.js')
+  , MemoryCache = require('../lib/docserver/memoryCache.js')
+  ;
+
+
+var BASE_DIR = 'docs';
+var EXTENSIONS = ['.md', '.mdown'];
+
+
+test('throw a 404 using a template', function(t) {
+  renderError(404, 'docs/goodbye/cruel', BASE_DIR, EXTENSIONS,
+    function(err, headers, body)
+  {
+    t.notOk(err, 'should complete without error');
+    t.ok('Content-Type' in headers, 'Content-Type header should be set');
+    t.equal(headers['Content-Type'], 'text/html; charset=UTF-8',
+      'Content-Type should be text/html; charset=UTF-8');
+    var content = fs.readFileSync('docs/goodbye/404-rendered.html');
+    t.equal(body, content.toString(),
+      'should render template as expected');
+    t.end();
+  });
+});
+
+test('throw a 404 not using a template', function(t) {
+  renderError(404, 'docs', BASE_DIR, EXTENSIONS, function(err, headers, body)
+  {
+    t.notOk(err, 'should complete without error');
+    t.ok('Content-Type' in headers, 'Content-Type header should be set');
+    t.equal(headers['Content-Type'], 'text/plain',
+      'Content-Type should be text/plain');
+    t.equal(body, '404', 'should render as just the error code');
+    t.end();
+  });
+});
